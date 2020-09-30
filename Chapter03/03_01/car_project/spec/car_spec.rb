805340721c0ecaabf574ccb81c5f323b7139d38b
Ben <ben.dalton@madetech.com>
2020-09-30 22:48:37 +0100

    wrote tests for full_name with no arguments

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -38,7 +38,16 @@ describe 'Car' do
     describe '#full_name' do
         it "returns a string in the expected format" do
             @honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
-            expect(@honda.full_name).to eq('2004 Honda (blue)')
+            expect(@honda.full_name).to eq("2004 Honda (blue)")
+        end
+
+        context "when initialized with no arguments" do
+            it "returns a string using default values" do
+                car = Car.new
+                expect(car.full_name).to eq("2007 Volvo (unknown)")
+            end
         end
     end
+
+
 end
