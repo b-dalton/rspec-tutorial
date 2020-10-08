b81079322518000878652fb1610891055b5b2836
Ben <ben.dalton@madetech.com>
2020-10-08 21:00:47 +0100

    add let methods to .colors and full_name tests

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -34,22 +34,26 @@ describe 'Car' do
     end
 
     describe '.colors' do
+
+        let(:colors) { ['blue', 'black', 'red', 'green'] }
+
         it "returns an array of color names" do
-            c = ['blue', 'black', 'red', 'green']
-            expect(Car.colors).to match_array(c)
+            expect(Car.colors).to match_array(colors)
         end
     end
 
     describe '#full_name' do
+
+        let(:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue') }
+        let(:new_car) { Car.new }
+
         it "returns a string in the expected format" do
-            @honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
-            expect(@honda.full_name).to eq("2004 Honda (blue)")
+            expect(honda.full_name).to eq("2004 Honda (blue)")
         end
 
         context "when initialized with no arguments" do
             it "returns a string using default values" do
-                car = Car.new
-                expect(car.full_name).to eq("2007 Volvo (unknown)")
+                expect(new_car.full_name).to eq("2007 Volvo (unknown)")
             end
         end
     end
