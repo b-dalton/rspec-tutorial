6dabf1fb076d9111f70cf138b85517b23cb64ed8
Ben <ben.dalton@madetech.com>
2020-10-08 20:57:09 +0100

    add let method to attributes tests

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -4,27 +4,30 @@ describe 'Car' do
     
     describe 'attributes' do
 
-        before(:example) do
-            @car = Car.new
-        end
+        let(:car) { Car.new }
+        # "let" is better than "before" for 
+        # setting up instance variables
+        # before(:example) do
+        #     @car = Car.new
+        # end
         
         it "allows reading and writing for :make" do
-            @car.make = "Test"
-            expect(@car.make).to eq("Test")
+            car.make = "Test"
+            expect(car.make).to eq("Test")
         end
 
         it "allows reading and writing for :year" do
-            @car.year = 9999
-            expect(@car.year).to eq(9999)
+            car.year = 9999
+            expect(car.year).to eq(9999)
         end
 
         it "allows reading and writing for :color" do
-            @car.year = "foo"
-            expect(@car.year).to eq("foo")
+            car.year = "foo"
+            expect(car.year).to eq("foo")
         end
 
         it "allows reading for :wheels" do
-            expect(@car.wheels).to eq(4)
+            expect(car.wheels).to eq(4)
         end
 
         it "allows writing for :doors" 
