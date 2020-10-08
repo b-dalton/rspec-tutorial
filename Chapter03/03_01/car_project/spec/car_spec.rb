e86fe0504693b48d932fe7968daf3baf4798ed11
Ben <ben.dalton@madetech.com>
2020-10-08 21:10:13 +0100

    undid changes, wrong file

--- Chapter03/03_01/car_project/spec/car_spec.rb
+++ Chapter03/03_01/car_project/spec/car_spec.rb
@@ -3,30 +3,27 @@ require 'car'
 describe 'Car' do
     
     describe 'attributes' do
-
-        let(:car) { Car.new }
-        # "let" is better than "before" for 
-        # setting up instance variables
-        # before(:example) do
-        #     @car = Car.new
-        # end
         
         it "allows reading and writing for :make" do
+            car = Car.new
             car.make = "Test"
             expect(car.make).to eq("Test")
         end
 
         it "allows reading and writing for :year" do
+            car = Car.new
             car.year = 9999
             expect(car.year).to eq(9999)
         end
 
         it "allows reading and writing for :color" do
+            car = Car.new
             car.year = "foo"
             expect(car.year).to eq("foo")
         end
 
         it "allows reading for :wheels" do
+            car = Car.new
             expect(car.wheels).to eq(4)
         end
 
@@ -34,26 +31,22 @@ describe 'Car' do
     end
 
     describe '.colors' do
-
-        let(:colors) { ['blue', 'black', 'red', 'green'] }
-
         it "returns an array of color names" do
-            expect(Car.colors).to match_array(colors)
+            c = ['blue', 'black', 'red', 'green']
+            expect(Car.colors).to match_array(c)
         end
     end
 
     describe '#full_name' do
-
-        let(:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue') }
-        let(:new_car) { Car.new }
-
         it "returns a string in the expected format" do
-            expect(honda.full_name).to eq("2004 Honda (blue)")
+            @honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
+            expect(@honda.full_name).to eq("2004 Honda (blue)")
         end
 
         context "when initialized with no arguments" do
             it "returns a string using default values" do
-                expect(new_car.full_name).to eq("2007 Volvo (unknown)")
+                car = Car.new
+                expect(car.full_name).to eq("2007 Volvo (unknown)")
             end
         end
     end
