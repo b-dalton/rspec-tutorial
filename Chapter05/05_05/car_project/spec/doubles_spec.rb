76087992d266f0f122a41ce216cb1956e88e7442
Ben <ben.dalton@madetech.com>
2020-10-13 21:58:28 +0100

    add partial test doubles - allow stubbing instance methods on Ruby classes

--- Chapter05/05_05/car_project/spec/doubles_spec.rb
+++ Chapter05/05_05/car_project/spec/doubles_spec.rb
@@ -47,4 +47,15 @@ describe 'Doubles' do
         expect(die.roll).to eq(6) # continues returning last value
     end
 
+    context 'with partial test doubles' do
+        
+        it "allows stubbing instance methods on Ruby classes" do
+            time = Time.new(2010, 1, 1, 12, 0, 0)
+            allow(time).to receive(:year).and_return(1975)
+
+            expect(time.to_s).to eq("2010-01-01 12:00:00 -0500")
+            expect(time.year).to eq(1975)
+        end
+    end
+
 end
