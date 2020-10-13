763aaedfb5acf6919c6562047fc7394e077596e5
Ben <ben.dalton@madetech.com>
2020-10-13 21:38:08 +0100

    add remaining doubles tests

--- Chapter05/05_05/car_project/spec/doubles_spec.rb
+++ Chapter05/05_05/car_project/spec/doubles_spec.rb
@@ -7,23 +7,40 @@ describe 'Doubles' do
     end
 
     it "allow stubbing a response with a block" do
-        
+        dbl = double("Chant")
+        # When I say 'Hey!', you say 'Ho!'
+        allow(dbl).to receive(:hey!) { "Ho!" }
+        # "Hey!", "Ho!"
+        expect(dbl.hey!).to eq("Ho!")
     end
 
     it "allow stubbing responses with #and_return" do
-        
+        dbl = double("Chant")
+        # Preferred syntax
+        # When I say "Hey!", you say "Ho!"
+        allow(dbl).to receive(:hey!).and_return("Ho!")
+        # "Hey!", "Ho!"
+        expect(dbl.hey!).to eq("Ho!")
     end
 
     it "allow stubbing multiple methods with hash syntax" do
-        
+        dbl = double("Person", :full_name => "Mary Smith", :initials => "MTS")
+        expect(dbl.full_name).to eq("Mary Smith")
+        expect(dbl.initials).to eq("MTS")
     end
 
     it "allows stubbing with a hash argument to #double" do
-        
+
     end
 
     it "allow stubbing multiple responses with #and_return" do
-        
+        die = double("Die")
+        allow(die).to receive(:roll).and_return(1, 5, 2, 6)
+        expect(die.roll).to eq(1)
+        expect(die.roll).to eq(5)
+        expect(die.roll).to eq(2)
+        expect(die.roll).to eq(6)
+        expect(die.roll).to eq(6) # continues returning last value
     end
 
 end
