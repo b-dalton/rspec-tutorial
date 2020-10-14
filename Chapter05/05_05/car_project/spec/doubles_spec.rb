34afa4c13db175768853fb169ff6783f33e0adf3
Ben <ben.dalton@madetech.com>
2020-10-14 22:52:05 +0100

    add 'with spying abilities' tests

--- Chapter05/05_05/car_project/spec/doubles_spec.rb
+++ Chapter05/05_05/car_project/spec/doubles_spec.rb
@@ -225,4 +225,45 @@ describe 'Doubles' do
         end
     end
 
+    context 'with spying abilities' do
+        
+        it "can expect a call after it is received" do
+            dbl = spy("Chant")
+            allow(dbl).to receive(:hey!).and_return("Ho!")
+            dbl.hey!
+            expect(dbl).to have_received(:hey!)
+        end
+
+        it 'can use message constraints' do
+            dbl = spy("Chant")
+            allow(dbl).to receive(:hey!).and_return("Ho!")
+            dbl.hey!
+            dbl.hey!
+            dbl.hey!
+            expect(dbl).to have_received(:hey!).with(no_args).exactly(3).times
+        end
+
+        it ' can expect any message already sent to be declared a spy' do
+            customer = spy("Customer")
+            # Notice that we don't stub :send_invoice
+            # allow(customer).to receive(:send_invoice)
+            customer.send_invoice
+            expect(customer).to have_received(:send_invoice)
+        end
+
+        it 'can expect only allowed messages on partial doubles' do
+            class Customer
+                def send_invoice
+                    true
+                end
+            end
+            customer = Customer.new
+            # Must stub :send_invoice to start spying
+            allow(customer).to receive(:send_invoice)
+            customer.send_invoice
+            expect(customer).to have_received(:send_invoice)
+        end
+    end
+
+
 end
